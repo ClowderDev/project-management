abf4e59962dd841bdb865af81e5f72baeb139896
ClowderDev <tranlevinhbuu@gmail.com>
2025-08-25 14:10:42 +0700

    fix: fix analytics

--- frontend/app/root.tsx
+++ frontend/app/root.tsx
@@ -10,8 +10,7 @@ import {
 import type { Route } from "./+types/root";
 import "./app.css";
 import ReactQueryProvider from "./provider/react-query-provider";
-import { Analytics } from "@vercel/analytics/next";
-
+import { Analytics } from "@vercel/analytics/react";
 export const links: Route.LinksFunction = () => [
   { rel: "preconnect", href: "https://fonts.googleapis.com" },
   {
